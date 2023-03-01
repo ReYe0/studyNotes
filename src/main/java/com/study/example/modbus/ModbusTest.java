dd42b0c7369d90b7f3c8c61dda7bcc098a63153b
erye0 <1299461580@qq.com>
2023-03-01 08:58:25 +0800

    记得学bat

--- src/main/java/com/study/example/modbus/ModbusTest.java
+++ src/main/java/com/study/example/modbus/ModbusTest.java
@@ -20,34 +20,27 @@ public class ModbusTest {
                 // TCP参数设置ip地址
                 // tcpParameters.setHost(InetAddress.getLocalHost());
                 tcpParameters.setHost(adress);
-
                 // TCP设置长连接
                 tcpParameters.setKeepAlive(true);
                 // TCP设置端口，这里设置是默认端口502
                 tcpParameters.setPort(Modbus.TCP_PORT);
-
                 // 创建一个主机
                 ModbusMaster master = ModbusMasterFactory.createModbusMasterTCP(tcpParameters);
                 Modbus.setAutoIncrementTransactionId(true);
-
                 int slaveId = 1;//从机地址
                 int offset = 0;//寄存器读取开始地址
-                int quantity = 10;//读取的寄存器数量
-
-
+                int quantity = 23;//读取的寄存器数量
                 try {
                     if (!master.isConnected()) {
                         master.connect();// 开启连接
                     }
-
                     // 读取对应从机的数据，readInputRegisters读取的写寄存器，功能码04
                     int[] registerValues = master.readInputRegisters(slaveId, offset, quantity);
-
+//                    int[] registerValue2 = master.readHoldingRegisters(slaveId, offset, quantity);
                     // 控制台输出
                     for (int value : registerValues) {
                         System.out.println("Address: " + offset++ + ", Value: " + value);
                     }
-
                 } catch (ModbusProtocolException e) {
                     e.printStackTrace();
                 } catch (ModbusNumberException e) {
