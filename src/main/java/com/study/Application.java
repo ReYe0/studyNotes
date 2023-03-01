deb7b986646031a28b770d76919da0ad5953d02c
erye0 <1299461580@qq.com>
2023-03-01 13:03:39 +0800

    回家整理javaweb的笔记，之后整理spring;正在处理jdbc

--- src/main/java/com/study/Application.java
+++ src/main/java/com/study/Application.java
@@ -3,11 +3,16 @@ package com.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("");
+        connection.rollback();
         SpringApplication.run(Application.class,args);
     }
 }
