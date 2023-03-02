6a55ccab379b51c3836de752b766853ee2697698
erye0 <1299461580@qq.com>
2023-03-02 15:59:46 +0800

    回家整理javaweb的笔记，之后整理spring;正在处理jdbc

--- src/main/java/com/study/example/modbus/ModbusTest.java
+++ src/main/java/com/study/example/modbus/ModbusTest.java
@@ -9,24 +9,26 @@ import com.intelligt.modbus.jlibmodbus.master.ModbusMasterFactory;
 import com.intelligt.modbus.jlibmodbus.tcp.TcpParameters;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class ModbusTest {
     public static void main(String[] args) {
             try {
-                // 设置主机TCP参数
-                TcpParameters tcpParameters = new TcpParameters();
-                // 设置TCP的ip地址
-                InetAddress adress = InetAddress.getByName("192.168.0.7");
-                // TCP参数设置ip地址
-                // tcpParameters.setHost(InetAddress.getLocalHost());
-                tcpParameters.setHost(adress);
-                // TCP设置长连接
-                tcpParameters.setKeepAlive(true);
-                // TCP设置端口，这里设置是默认端口502
-                tcpParameters.setPort(Modbus.TCP_PORT);
-                // 创建一个主机
-                ModbusMaster master = ModbusMasterFactory.createModbusMasterTCP(tcpParameters);
-                Modbus.setAutoIncrementTransactionId(true);
+//                // 设置主机TCP参数
+//                TcpParameters tcpParameters = new TcpParameters();
+//                // 设置TCP的ip地址
+//                InetAddress adress = InetAddress.getByName("192.168.0.7");
+//                // TCP参数设置ip地址
+//                // tcpParameters.setHost(InetAddress.getLocalHost());
+//                tcpParameters.setHost(adress);
+//                // TCP设置长连接
+//                tcpParameters.setKeepAlive(true);
+//                // TCP设置端口，这里设置是默认端口502
+//                tcpParameters.setPort(Modbus.TCP_PORT);
+//                // 创建一个主机
+//                ModbusMaster master = ModbusMasterFactory.createModbusMasterTCP(tcpParameters);
+//                Modbus.setAutoIncrementTransactionId(true);
+                ModbusMaster master = getMaster("192.168.0.7");
                 int slaveId = 1;//从机地址
                 int offset = 0;//寄存器读取开始地址
                 int quantity = 23;//读取的寄存器数量
@@ -60,5 +62,29 @@ public class ModbusTest {
                 e.printStackTrace();
             }
 
+    }
+
+
+    public static ModbusMaster getMaster(String ip){
+        // 设置主机TCP参数
+        TcpParameters tcpParameters = new TcpParameters();
+        // 设置TCP的ip地址
+        InetAddress adress = null;
+        try {
+            adress = InetAddress.getByName(ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        // TCP参数设置ip地址
+        // tcpParameters.setHost(InetAddress.getLocalHost());
+        tcpParameters.setHost(adress);
+        // TCP设置长连接
+        tcpParameters.setKeepAlive(true);
+        // TCP设置端口，这里设置是默认端口502
+        tcpParameters.setPort(Modbus.TCP_PORT);
+        // 创建一个主机
+        ModbusMaster master = ModbusMasterFactory.createModbusMasterTCP(tcpParameters);
+        Modbus.setAutoIncrementTransactionId(true);
+        return master;
     }
 }
---
+++ src/test/java/com/study/test/OtherTest.java
@@ -0,0 +1,12 @@
+package com.study.test;
+
+import org.junit.Test;
+
+public class OtherTest {
+    @Test
+    public void test(){
+        String str = "2303";
+        System.out.println(str.substring(0, 2));
+        System.out.println(str.substring(2, 4));
+    }
+}
